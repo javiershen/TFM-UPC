c1ed9af89b0ebb36133f01c37acbbdd4bb6d74ba
Meritxell-UPC <81539002+Meritxell-UPC@users.noreply.github.com>
2022-11-18 19:09:49 +0100

    Add files via upload

---
+++ pharma/README.md
@@ -0,0 +1,125 @@
+# TFM-UPC Pharma
+
+This is a project simulating a pharma industry use case where blockchain can be applied to fasten the medical supply process and prevent medicament falsification.
+
+![Network](https://i.ibb.co/3v4KnGb/Screenshot-2022-11-17-at-21-40-03.png)
+
+## Prerequisites
+
+Check the documentation of Hyperledger Fabric, in order to download the required prerequisites to run the network: https://hlf.readthedocs.io/en/latest/prereqs.html
+
+## Set the network
+
+In order to set up the network, run the following commands in order to set and run the network.
+
+```
+$ cd TFM-UPC/pharma
+$ ./startPharma.sh
+```
+
+## Init the ledgers and prepare the CLI to run the commands
+
+```
+$ source ./setEnvVars.sh
+$ ./initLedgers.sh
+```
+
+## Login to the channels
+
+In order to log into, we have set some predefined users.
+
+To log in into the Pharmacy - Laboratory network set on channel 1, we have to run the following command. This command will create 3 inputs on the shell so the user can input the entity that he is part of, his username and his password.
+
+```
+$ source ./userLoginCOne.sh
+```
+
+In order to log into the channel 2, where there is a network between the Hospital and the Pharmacy, we have to run the following command.
+
+```
+$ source ./userLoginCTwo.sh
+```
+
+## Demo with the network
+
+In order to give a small test on our network, we are going to simulate the full process of a medicine from its creation un till it given to the user, showing its status on each point.
+
+![Demo](https://i.ibb.co/tJV8gdM/Screenshot-2022-11-17-at-22-41-13.png)
+
+### Medicine registration
+
+First of all, we need to log in with the lab user or lab admin so we can register the medicine in channel 1.
+
+In order to register the medicine, we execute the following command and select the option 1.
+
+```
+$ ./userLabFunctions.sh
+1) 1- Register a medicament
+2) 2- Send a medicament to a pharmacy
+3) 3- Quit
+Which of the following actions you want to do: 1
+```
+
+Once we selected the option, we have to introduce the medicine data: Medicine Name, the medicine product code, its serial number, the lot number, the expiration year and the expiration month.
+
+### Read created medicine / Read all users / Read all medicines
+
+Once we have the medicine registered, and logged in with the admin user, we can check its information and also the information about all the medicines and users related to the pharmacy.
+
+```
+$ ./adminLabFunctions.sh
+1) 1- Read all medicaments    3) 3- Read a medicament info
+2) 2- Read all users          4) 4- Quit
+```
+
+### Send medicine to pharmacy
+
+As we did before, we have to be logged in with one of the lab users. Then we select the option 2 and input the medicine product code.
+
+```
+$ ./userLabFunctions.sh
+1) 1- Register a medicament
+2) 2- Send a medicament to a pharmacy
+3) 3- Quit
+Which of the following actions you want to do: 2
+```
+
+### Notify received medicine
+
+When the medicine is received by the pharmacy. The pharmacy, throught a pharmacy user or admin, will have to notify that it was received to both channels. Therefore, the pharmacy user needs to be logged on both channels. 
+
+```
+$ ./userPharmacyFunctions.sh
+1) 1- Receive a medicament   3) 3- Use prescription
+2) 2- Dispense a medicament  4) 4- Quit
+Which of the following actions you want to do: 1
+```
+
+### Create medicine prescription
+
+Once the medicine is recieved by the pharmacy and notified to the pharmacy-hospital channel2, at the hospital side you will have to create a prescription in order to allow its dispatchment.
+
+To do so, first we will have to log in with a member of the hospital (user or admin), then the hospital member will be able to generate a prescription for the patient.
+1- Generate prescription" "3- Quit"
+```
+$ ./userSanitaryFunctions.sh
+1) 1- Generate prescription
+2) 2- See prescription 
+3) 3- Quit
+Which of the following actions you want to do: 1
+```
+
+### User receives the medicine (Consume prescription and medicine dispatchment)
+
+Once the prescription is generated and received at the channel, the patient will be able to go to the pharmacy and ask for the medicine that was asigned to him. In order to get the medicine, the prescription will be consumed and the medicine will be dispatched from the pharmacy.
+
+```
+$ ./userPharmacyFunctions.sh
+1) 1- Receive a medicament   3) 3- Use prescription
+2) 2- Dispense a medicament  4) 4- Quit
+Which of the following actions you want to do:
+```
+
+### More information
+
+For deeper knowledge of the network we have built, its configuration and its chaincode, we also complemented the project with written memory that was delivered to our teacher.
--- pharma/userPharmacyFunctions.sh
+++ pharma/userPharmacyFunctions.sh
@@ -11,30 +11,23 @@ popd () {
 pushd ../test-network
 
 PS3='Which of the following actions you want to do: '
-options=("1- Receive a medicament" "2- Dispense a medicament" "3- Use prescription" "4- Quit")
+options=("1- Receive a medicament" "2- Use prescription " "3- Dispense a medicament" "4- Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "1- Receive a medicament")
-            echo MEDICAMENT CODE:
+            echo MEDICAMENT SERIAL NUMBER:
             read medicament_code
             export MEDICAMENT_CODE="$medicament_code"
             echo Enter Medicine Name:
             read medicine_name
             export MEDICINE_NAME="$medicine_name"           
-            peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.pharma.com --tls --cafile "${PWD}/organizations/ordererOrganizations/pharma.com/orderers/orderer.pharma.com/msp/tlscacerts/tlsca.pharma.com-cert.pem" -C channel1 -n basic1 --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.pharma.com/peers/peer0.org1.pharma.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.pharma.com/peers/peer0.org2.pharma.com/tls/ca.crt" -c '{"function":"Invoke","Args":["ReceiveMedicament", "'"${USERNAME_C1}"'", "'"${ENTITY_ID_C1}"'", "'"${PRODUCT_CODE}"'"]}'
+            peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.pharma.com --tls --cafile "${PWD}/organizations/ordererOrganizations/pharma.com/orderers/orderer.pharma.com/msp/tlscacerts/tlsca.pharma.com-cert.pem" -C channel1 -n basic1 --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.pharma.com/peers/peer0.org1.pharma.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.pharma.com/peers/peer0.org2.pharma.com/tls/ca.crt" -c '{"function":"Invoke","Args":["ReceiveMedicament", "'"${USERNAME_C1}"'", "'"${ENTITY_ID_C1}"'", "'"${MEDICAMENT_CODE}"'"]}'
               
            peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.pharma.com --tls --cafile "${PWD}/organizations/ordererOrganizations/pharma.com/orderers/orderer.pharma.com/msp/tlscacerts/tlsca.pharma.com-cert.pem" -C channel2 -n basic2 --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.pharma.com/peers/peer0.org1.pharma.com/tls/ca.crt" --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.pharma.com/peers/peer0.org3.pharma.com/tls/ca.crt" -c '{"function":"Invoke","Args":["AddMedicamentToStock", "'"${USERNAME_C2}"'", "'"${ENTITY_ID_C2}"'", "'"${MEDICAMENT_CODE}"'", "'"${MEDICINE_NAME}"'"]}'
             break
             ;;
-        "2- Dispense a medicament")
-            echo MEDICAMENT CODE:
-            read medicament_code
-            export MEDICAMENT_CODE="$medicament_code"
-            peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.pharma.com --tls --cafile "${PWD}/organizations/ordererOrganizations/pharma.com/orderers/orderer.pharma.com/msp/tlscacerts/tlsca.pharma.com-cert.pem" -C channel1 -n basic1 --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.pharma.com/peers/peer0.org1.pharma.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.pharma.com/peers/peer0.org2.pharma.com/tls/ca.crt" -c '{"function":"Invoke","Args":["DispenseMedicament", "'"${USERNAME_C1}"'", "'"${ENTITY_ID_C1}"'", "'"${MEDICAMENT_CODE}"'"]}'
-            break
-            ;;
-        "3- Use prescription")
+        "2.- Use prescription")
             echo MEDICAMENT CODE:
             read medicament_code
             export MEDICAMENT_CODE="$medicament_code"
@@ -42,6 +35,13 @@ do
             read patient_ID
             export PATIENT_ID="$patient_ID"
             peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.pharma.com --tls --cafile "${PWD}/organizations/ordererOrganizations/pharma.com/orderers/orderer.pharma.com/msp/tlscacerts/tlsca.pharma.com-cert.pem" -C channel2 -n basic2 --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.pharma.com/peers/peer0.org1.pharma.com/tls/ca.crt" --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.pharma.com/peers/peer0.org3.pharma.com/tls/ca.crt" -c '{"function":"Invoke","Args":["ConsumePrescription", "'"${USERNAME_C2}"'", "'"${ENTITY_ID_C2}"'", "'"${MEDICAMENT_CODE}"'", "'"${PATIENT_ID}"'"]}'
+            break
+            ;;
+        "3- Dispense a medicament")
+            echo MEDICAMENT SERIAL NUMBER:
+            read medicament_code
+            export MEDICAMENT_CODE="$medicament_code"
+            peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.pharma.com --tls --cafile "${PWD}/organizations/ordererOrganizations/pharma.com/orderers/orderer.pharma.com/msp/tlscacerts/tlsca.pharma.com-cert.pem" -C channel1 -n basic1 --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.pharma.com/peers/peer0.org1.pharma.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.pharma.com/peers/peer0.org2.pharma.com/tls/ca.crt" -c '{"function":"Invoke","Args":["DispenseMedicament", "'"${USERNAME_C1}"'", "'"${ENTITY_ID_C1}"'", "'"${MEDICAMENT_CODE}"'"]}'
             break
             ;;
         "4- Quit")
