cabc99716a905cd32b986152e1bfbfd294947e9f
Meritxell-UPC <81539002+Meritxell-UPC@users.noreply.github.com>
2022-11-18 18:49:15 +0100

    Add files via upload

--- pharma/adminLabFunctions.sh
+++ pharma/adminLabFunctions.sh
@@ -24,7 +24,7 @@ do
             break
             ;;
         "3- Read a medicament info")
-            echo READ PRODUCT CODE:
+            echo MEDICAMENT SERIAL NUMBER:
             read read_product_code
             export READ_PRODUCT_CODE="$read_product_code"
             peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.pharma.com --tls --cafile "${PWD}/organizations/ordererOrganizations/pharma.com/orderers/orderer.pharma.com/msp/tlscacerts/tlsca.pharma.com-cert.pem" -C channel1 -n basic1 --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.pharma.com/peers/peer0.org1.pharma.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.pharma.com/peers/peer0.org2.pharma.com/tls/ca.crt" -c '{"function":"GetMedicament","Args":["'"${USERNAME_C1}"'", "'"${ENTITY_ID_C1}"'", "'"${READ_PRODUCT_CODE}"'"]}'
